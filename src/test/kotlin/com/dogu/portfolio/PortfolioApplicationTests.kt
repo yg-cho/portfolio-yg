aabf8d629a1bdeb3eedca6d892fc996cb89d801b
yg-cho <cyg4484@gmail.com>
2024-07-22 09:26:20 +0900

    Github setting

---
+++ src/test/kotlin/com/dogu/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dogu.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
