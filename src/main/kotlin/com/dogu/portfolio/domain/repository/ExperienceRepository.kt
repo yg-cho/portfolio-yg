ab4ec63033edb4c09e775b245a44000a628b2bca
yg-cho <cyg4484@gmail.com>
2024-07-23 17:32:44 +0900

    Feat: Repository refactoring

--- src/main/kotlin/com/dogu/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/dogu/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,8 +2,14 @@ package com.dogu.portfolio.domain.repository
 
 import com.dogu.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.Optional
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive:Boolean): List<Experience>
+
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    override fun findById(id: Long): Optional<Experience>
 }
