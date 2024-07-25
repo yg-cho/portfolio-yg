d5731db162c28da41674b3543e1330d129b328e3
yg-cho <cyg4484@gmail.com>
2024-07-25 14:52:10 +0900

    Feat: add Repository

--- src/main/kotlin/com/dogu/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/dogu/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,7 +1,39 @@
 package com.dogu.portfolio.presentation.repository
 
+import com.dogu.portfolio.domain.entity.*
+import com.dogu.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository {
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectRepository: ProjectRepository,
+    private val experienceRepository: ExperienceRepository
+) {
+
+    fun getActiveAchievements():List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+    fun getActiveExperiences():List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+    fun getActiveIntroductions():List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+    fun getActiveLinks():List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+    fun getActiveSkills():List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+    fun getActiveProjects():List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+
+
+
 }
