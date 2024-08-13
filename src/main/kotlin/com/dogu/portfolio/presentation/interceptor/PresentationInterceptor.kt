c98d74ef0a173e27ab982915aaccd6f2bbd12dbd
yg-cho <cyg4484@gmail.com>
2024-08-13 11:19:53 +0900

    Add: Interceptor

--- src/main/kotlin/com/dogu/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/dogu/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,25 @@
 package com.dogu.portfolio.presentation.interceptor
 
+import com.dogu.portfolio.domain.entity.HttpInterface
+import com.dogu.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+):HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
