a2c44071317c187f35151d588a01872e4c56f24c
yg-cho <sorisori01@naver.com>
2024-08-26 22:37:41 +0900

    Feat: Common Interceptor

--- src/main/kotlin/com/dogu/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/dogu/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -8,5 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class AdminInterceptorConfiguration(
     val adminInterceptor: AdminInterceptor
 ): WebMvcConfigurer {
-    override fun addInterceptors(registry: InterceptorRegistry) {}
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "h2**")
+    }
 }
