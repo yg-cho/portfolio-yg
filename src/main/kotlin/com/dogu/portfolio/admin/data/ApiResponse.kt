9a672c8987d2253c7eecf9c980eddc9dbb32e956
yg-cho <sorisori01@naver.com>
2024-08-26 22:31:32 +0900

    Feat: Common DTO

--- src/main/kotlin/com/dogu/portfolio/admin/data/ApiResponse.kt
+++ src/main/kotlin/com/dogu/portfolio/admin/data/ApiResponse.kt
@@ -4,4 +4,22 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+
+    companion object {
+
+        fun successCreate(): ResponseEntity<Any> {
+            return ResponseEntity.ok().body("데이터가 저장되었습니다.")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ResponseEntity.ok().body("데이터가 수정되었습니다.")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ResponseEntity.ok().body("데이터가 삭제되었습니다.")
+        }
+
+
+
+    }
 }
--- src/main/kotlin/com/dogu/portfolio/admin/data/TableDTO.kt
+++ src/main/kotlin/com/dogu/portfolio/admin/data/TableDTO.kt
@@ -1,8 +1,44 @@
 package com.dogu.portfolio.admin.data
 
+import jakarta.persistence.Table
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
+
 class TableDTO(
     val name: String,
     val columns: List<String>,
     val records: List<List<String>>
 ) {
+    companion object {
+        fun<T: Any> from(classInfo: KClass<T>, entities: List<Any>, vararg filterings: String): TableDTO {
+            val name = classInfo.simpleName ?: "UnKnown"
+            val columns = createColumns(classInfo, *filterings)
+            var records = entities.map { entity ->
+                columns.map { column ->
+                    classInfo.memberProperties
+                        .find { column.equals(it.name) }
+                        ?.getter
+                        ?.call(entity)
+                        .toString()
+                }.toList()
+            }.toList()
+
+            return TableDTO(name = name, columns = columns, records = records)
+        }
+
+        private fun<T:Any> createColumns(classInfo: KClass<T>, vararg filterings: String): MutableList<String> {
+            val mainColumns = classInfo.java.declaredFields
+                .filter { !filterings.contains(it.name) }
+                .map { it.name }
+                .toMutableList()
+
+            val baseColumns = classInfo.java.superclass.declaredFields
+                .map { it.name }
+                .toMutableList()
+
+            return (mainColumns + baseColumns).toMutableList()
+        }
+
+    }
+
 }
